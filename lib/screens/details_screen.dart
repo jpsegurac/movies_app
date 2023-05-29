fe993032addde8c005c7c03c971029faf3de1611
Jean <jpsc1117@gmail.com>
2023-05-28 21:21:51 -0500

    Set up my movies apps homescreen ands details screen

---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Anim tempor ea pariatur quis duis et exercitation voluptate excepteur deserunt deserunt fugiat.')
+      ),
+    );
+  }
+}
